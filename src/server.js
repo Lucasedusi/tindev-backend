194b8e72337ea75ff725c2f5a6d7e4d8af5b9a24
Lucasedusi <lucasmto.16@hotmail.com>
2020-02-18 14:21:40 -0300

    Add socket.io

--- src/server.js
+++ src/server.js
@@ -5,13 +5,19 @@ const cors = require('cors')
 
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  console.log('nova conexao', socket.id);
+});
 
 mongoose.connect('mongodb+srv://tindev:tindev@cluster0-k5spm.mongodb.net/omnistack8?retryWrites=true&w=majority', {
   useNewUrlParser: true,
 });
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
